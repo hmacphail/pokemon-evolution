d30eb82b3088942979f95a9ddbdae1cc07b4cb9f
Heather MacPhail <hmacphail@gmail.com>
2017-01-27 21:52:13 -0800

    Postgres connection

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 // set up ========================
 var express  = require('express');
 var app      = express();                               // create our app w/ express
-//var mongoose = require('mongoose');                     // mongoose for mongodb
+var pg = require('pg');                    // postgres db connection
 var morgan = require('morgan');             // log requests to the console (express4)
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
 // configuration =================
 
-//mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
+//pg.defaults.ssl = true;
+var host = 'ec2-54-83-194-208.compute-1.amazonaws.com';
+var database = 'd8dvbdrannbitc';
+var user = 'cganvxfjtvuhcn';
+var port = 5432;
+
+var config = {
+  host: host,
+  port: port,
+  database: database,
+  user: user,
+  password: '6924217d33a6155b7efcd96c287668377e1da08bf29148f593f1f4ef2644de8c',
+  ssl: true
+};
+console.log(process.env.DATABASE_URL);
+pg.connect(config, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                                         // log every request to the console
@@ -20,6 +44,8 @@ app.use(methodOverride());
 // listen (start app with node server.js) ======================================
 app.listen(8080);
 console.log("App listening on port 8080");
+
+
 
 // index.js
 /*
