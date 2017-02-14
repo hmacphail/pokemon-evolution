21e95b69bcf9c2e1a93298e59b7a26c6ed9f60aa
Heather MacPhail <hmacphail@gmail.com>
2017-02-13 21:05:45 -0800

    Add description to items table

--- server/models/item.js
+++ server/models/item.js
@@ -4,6 +4,9 @@ module.exports = function(sequelize, DataTypes) {
   var Item = sequelize.define('item', {
     name: {
       type: Sequelize.STRING(20)
+    },
+    description: {
+      type: Sequelize.STRING(255)
     }
   }, {
     timestamps: false,
